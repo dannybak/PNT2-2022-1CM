41115ea9e2bc0e22cd67bbf5352fe7c85ae5e6e2
dannybak <danielbak10@gmail.com>
2022-03-21 22:34:41 -0300

    Modificando el TP (clases por array de objetos)

--- TP_CLASE2/scaloneta.js
+++ TP_CLASE2/scaloneta.js
@@ -1,81 +1,94 @@
-class Seleccion {
-    constructor(){
-        this.seleccion = [];
-    }
-
-    agregar(jugador) {
-        this.seleccion.push(jugador); 
-    }
-
-    mostrarJugadores() {
-        console.log(this.seleccion);
-    }
-
-    getSeleccion(){
-        return this.seleccion;
-    }
-}
-
-class Jugador {
-    constructor(id, nombreCompleto, posicion, edad) {
-        this.id = id;
-        this.nombreCompleto = nombreCompleto;
-        this.posicion = posicion;
-        this.edad = edad;
-    }
-}
-
-const scaloneta = new Seleccion();
-
-let jugador1 = new Jugador(23, "Damián Emiliano Martinez", "Arquero", 29);
-let jugador2 = new Jugador(3, "Nicolás Alejandro Tagliafico ", "Defensor", 29);
-let jugador3 = new Jugador(13, "Cristian Gabriel Romero", "Defensor", 23);
-let jugador4 = new Jugador(19, "Nicolás Otamendi", "Defensor", 34);
-let jugador5 = new Jugador(8, "Marcos Javier Acuña", "Defensor", 30);
-let jugador6 = new Jugador(7, "Rodrigo Javier De Paul", "Mediocampista", 27);
-let jugador7 = new Jugador(5, "Leandro Daniel Paredes", "Mediocampista", 27);
-let jugador8 = new Jugador(20, "Giovani Lo Celso", "Mediocampista", 25);
-let jugador9 = new Jugador(11, "Ángel Fabián Di María", "Delantero", 34);
-let jugador10 = new Jugador(10, "Lionel Andrés Messi", "Delantero", 34);
-let jugador11 = new Jugador(22, "Lautaro Javier Martinez", "Delantero", 24);
-
-scaloneta.agregar(jugador1);
-scaloneta.agregar(jugador2);
-scaloneta.agregar(jugador3);
-scaloneta.agregar(jugador4);
-scaloneta.agregar(jugador5);
-scaloneta.agregar(jugador6);
-scaloneta.agregar(jugador7);
-scaloneta.agregar(jugador8);
-scaloneta.agregar(jugador9);
-scaloneta.agregar(jugador10);
-scaloneta.agregar(jugador11);
+const scaloneta = 
+    [
+        {
+            id: 23,
+            nombreCompleto: 'Damián Emiliano Martinez',
+            posicion: 'Arquero',
+            edad: 29
+        },
+        {
+            id: 3,
+            nombreCompleto: 'Nicolás Alejandro Tagliafico',
+            posicion: 'Defensor',
+            edad: 29
+        },
+        {
+            id: 13,
+            nombreCompleto: 'Cristian Gabriel Romero',
+            posicion: 'Defensor',
+            edad: 23
+        },
+        {
+            id: 19,
+            nombreCompleto: 'Nicolás Otamendi',
+            posicion: 'Defensor',
+            edad: 34
+        },
+        {
+            id: 8,
+            nombreCompleto: 'Marcos Javier Acuña',
+            posicion: 'Defensor',
+            edad: 30
+        },
+        {
+            id: 7,
+            nombreCompleto: 'Rodrigo Javier De Paul',
+            posicion: 'Mediocampista',
+            edad: 27
+        },
+        {
+            id: 5,
+            nombreCompleto: 'Leandro Daniel Paredes',
+            posicion: 'Mediocampista',
+            edad: 27
+        },
+        {
+            id: 20,
+            nombreCompleto: 'Giovani Lo Celso',
+            posicion: 'Mediocampista',
+            edad: 25
+        },
+        {
+            id: 11,
+            nombreCompleto: 'Ángel Fabián Di María',
+            posicion: 'Delantero',
+            edad: 34
+        },
+        {
+            id: 10,
+            nombreCompleto: 'Lionel Andrés Messi',
+            posicion: 'Delantero',
+            edad: 34
+        },
+        {
+            id: 22,
+            nombreCompleto: 'Lautaro Javier Martinez',
+            posicion: 'Delantero',
+            edad: 24
+        }
+    ]
 
 //--> I) Mostrar por consola todos el array
-scaloneta.mostrarJugadores()
+console.log(scaloneta)
 
 //--> II) Hacer una busqueda por id con el html y mostrar el objeto encontrado por consola
 const buscarPorId = () => {
     const idAComparar = document.getElementById("idJugador").value;
-    let jugadores = scaloneta.getSeleccion()
 
-    let idEncontrado = jugadores.find(jug => jug.id == idAComparar);
+    let idEncontrado = scaloneta.find(jug => jug.id == idAComparar);
 
     idEncontrado ? console.log(idEncontrado.nombreCompleto) : console.log("No se encontró dicho jugador");
 }
 
 //--> III) Hacer un nuevo array solo con los nombres de los objetos y mostrar por consola
-const nombresCompletos = scaloneta.getSeleccion().map(j => j.nombreCompleto);
+const nombresCompletos = scaloneta.map(j => j.nombreCompleto);
 console.log(nombresCompletos)
 
 //--> IV) Hacer un promedio de edades/anios
-const edades = scaloneta.getSeleccion().map(j => j.edad);
-const cantJugadores = scaloneta.getSeleccion().length;
-
-const promedio = edades.reduce((valPrevio, valActual) => valPrevio + valActual, 0) / cantJugadores;
-
+const edades = scaloneta.map(j => j.edad);
+const promedio = edades.reduce((a, b) => a + b, 0) / scaloneta.length;
 console.log(promedio)
 
-//--> V) Hacer un nuevo array solo con los objetos de cierrto tipo(especie = alien o genero = terror, etc).
-const delanteros = scaloneta.getSeleccion().filter(pos => pos.posicion == "Delantero")  
+//--> V) Hacer un nuevo array solo con los objetos de cierto tipo
+const delanteros = scaloneta.filter(pos => pos.posicion == "Delantero")  
 console.log(delanteros)
