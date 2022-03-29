f7ad5bed14ff35a19ab1c3e06ab3fef95f005f62
dannybak <danielbak10@gmail.com>
2022-03-29 02:20:41 -0300

    Subiendo TP de la clase 3

---
+++ TP_CLASE3/tpClase3.js
@@ -0,0 +1,328 @@
+const scaloneta = 
+    [
+        {
+            id: '1',
+            dorsal: '1',
+            nombreCompleto: 'Damián Emiliano Martinez',
+            posicion: 'Arquero',
+            lugarNacimiento: 'Buenos Aires, Argentina',
+            edad: '29',
+            img: 'https://media.ambito.com/p/1f18cfae8b8931bc3819c7b60e2c15a5/adjuntos/239/imagenes/039/368/0039368766/1200x1200/smart/dibu-martinezjpg.jpg'
+        },
+        {
+            id: '2',
+            dorsal: '1',
+            nombreCompleto: 'Franco Armani',
+            posicion: 'Arquero',
+            lugarNacimiento: 'Santa Fe, Argentina',
+            edad: '35',
+            img: 'https://media.tycsports.com/files/2021/09/30/339272/armani-_w416.jpg'
+        },
+        {
+            id: '3',
+            dorsal: '3',
+            nombreCompleto: 'Nicolás Alejandro Tagliafico',
+            posicion: 'Defensor',
+            lugarNacimiento: 'Buenos Aires, Argentina',
+            edad: '29',
+            img: 'https://media.ambito.com/p/4078fa2279dd2062205f3774a4e01590/adjuntos/239/imagenes/037/658/0037658972/1200x900/smart/nicolas-tagliafico.jpg'
+        },
+        {
+            id: '4',
+            dorsal: '6',
+            nombreCompleto: 'Germán Pezzella',
+            posicion: 'Defensor',
+            lugarNacimiento: 'Buenos Aires, Argentina',
+            edad: '30',
+            img: 'https://www.benditofutbol.com/files/article_main/uploads/2020/03/14/5e6d309a9d24a.jpeg'
+        },
+        {
+            id: '5',
+            dorsal: '13',
+            nombreCompleto: 'Cristian Gabriel Romero',
+            posicion: 'Defensor',
+            lugarNacimiento: 'Cordoba, Argentina',
+            edad: '23',
+            img: 'https://media.ambito.com/p/6371cb7e1ee61a7085d0f225b2fc961d/adjuntos/239/imagenes/039/141/0039141633/1200x1200/smart/cuti-romerojpg.jpg'
+        },
+        {
+            id: '6',
+            dorsal: '19',
+            nombreCompleto: 'Nicolás Otamendi',
+            posicion: 'Defensor',
+            lugarNacimiento: 'Buenos Aires, Argentina',
+            edad: '34',
+            img: 'https://media.a24.com/p/25a69343f15865d318ca7073f0cb9178/adjuntos/296/imagenes/008/832/0008832825/1200x1200/smart/nicolas-otamendi-asaltojpg.jpg'
+        },
+        {
+            id: '7',
+            dorsal: '8',
+            nombreCompleto: 'Marcos Javier Acuña',
+            posicion: 'Defensor',
+            lugarNacimiento: 'Neuquen, Argentina',
+            edad: '30',
+            img: 'https://media.tycsports.com/files/2020/10/12/126155/acuna.jpg'
+        },
+        {
+            id: '8',
+            dorsal: '2',
+            nombreCompleto: 'Lucas Martínez Quarta',
+            posicion: 'Defensor',
+            lugarNacimiento: 'Buenos Aires, Argentina',
+            edad: '25',
+            img: 'https://upload.wikimedia.org/wikipedia/commons/9/94/Palestino_-_River_Plate_20190424_28.jpg'
+        },
+        {
+            id: '9',
+            dorsal: '4',
+            nombreCompleto: 'Juan Marcos Foyth',
+            posicion: 'Defensor',
+            lugarNacimiento: 'Buenos Aires, Argentina',
+            edad: '24',
+            img: 'http://deportebox.com/wp-content/uploads/2019/06/64862566_2392358434147901_2486911219731005440_n.jpg'
+        },
+        {
+            id: '10',
+            dorsal: '12',
+            nombreCompleto: 'Nahuel Molina Lucero',
+            posicion: 'Defensor',
+            lugarNacimiento: 'Cordoba, Argentina',
+            edad: '23',
+            img: 'https://upload.wikimedia.org/wikipedia/commons/f/f8/Universidad_Cat%C3%B3lica_-_Rosario_Central_20190313_04.jpg'
+        }, 
+        {
+            id: '11',
+            dorsal: '24',
+            nombreCompleto: 'Manuel Lanzini',
+            posicion: 'Mediocampista',
+            lugarNacimiento: 'Buenos Aires, Argentina',
+            edad: '29',
+            img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Hertha_BSC_vs._West_Ham_United_20190731_%28147%29.jpg/1200px-Hertha_BSC_vs._West_Ham_United_20190731_%28147%29.jpg'
+        },
+        {
+            id: '12',
+            dorsal: '18',
+            nombreCompleto: 'Guido Rodríguez',
+            posicion: 'Mediocampista',
+            lugarNacimiento: 'Buenos Aires, Argentina',
+            edad: '27',
+            img: 'https://imagenes.elpais.com/resizer/JDrh7ohgWaoZLQ8LdGz6lO2MxaQ=/414x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/7QRHWXVZ3N4USP7REPGHTHLMA4.jpg'
+        },
+        {
+            id: '13',
+            dorsal: '7',
+            nombreCompleto: 'Rodrigo Javier De Paul',
+            posicion: 'Mediocampista',
+            lugarNacimiento: 'Buenos Aires, Argentina',
+            edad: '27',
+            img: 'https://resizer.glanacion.com/resizer/oa9VAsdKVJeNDPArzB57os68ReI=/768x0/filters:format(webp):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/HZY7RV5TDFDLZLBSQZR523CWTM.JPG'
+        },
+        {
+            id: '14',
+            dorsal: '5',
+            nombreCompleto: 'Leandro Daniel Paredes',
+            posicion: 'Mediocampista',
+            lugarNacimiento: 'Buenos Aires, Argentina',
+            edad: '27',
+            img: 'https://media.tycsports.com/files/2021/06/17/295588/leandro-paredes_1440x810_wmk.jpg?v=4'
+        },
+        {
+            id: '15',
+            dorsal: '20',
+            nombreCompleto: 'Giovani Lo Celso',
+            posicion: 'Mediocampista',
+            lugarNacimiento: 'Santa Fe, Argentina',
+            edad: '25',
+            img: 'https://pbs.twimg.com/profile_images/1140687503743229952/JUE85s3G_400x400.jpg'
+        },
+        {
+            id: '16',
+            dorsal: '17',
+            nombreCompleto: 'Lucas Ariel Ocampos Lain',
+            posicion: 'Mediocampista',
+            lugarNacimiento: 'Buenos Aires, Argentina',
+            edad: '27',
+            img: 'https://www.elfutbolero.com.ar/__export/1618612821065/sites/elfutboleromx/img/2021/04/16/1_42.jpeg_803910648.jpeg'
+        },
+        {
+            id: '17',
+            dorsal: '15',
+            nombreCompleto: 'Nicolás Iván González',
+            posicion: 'Mediocampista',
+            lugarNacimiento: 'Buenos Aires, Argentina',
+            edad: '23',
+            img: 'https://media.tycsports.com/files/2021/06/14/293990/nico-gonzalez-seleccion-argentina_1440x810_wmk.jpg?v=13'
+        },
+        {
+            id: '18',
+            dorsal: '14',
+            nombreCompleto: 'Exequiel Alejandro Palacios',
+            posicion: 'Mediocampista',
+            lugarNacimiento: 'Tucuman, Argentina',
+            edad: '23',
+            img: 'https://pbs.twimg.com/profile_images/1271677845820051458/WFT72qyj_400x400.jpg'
+        },
+        {
+            id: '19',
+            dorsal: '28',
+            nombreCompleto: 'Alexis Mac Allister',
+            posicion: 'Mediocampista',
+            lugarNacimiento: 'La Pampa, Argentina',
+            edad: '23',
+            img: 'https://cloudfront-eu-central-1.images.arcpublishing.com/diarioas/IWTGRJK7NJOYPBNTCRXUJITXE4.jpg'
+        },
+        {
+            id: '20',
+            dorsal: '11',
+            nombreCompleto: 'Ángel Fabián Di María',
+            posicion: 'Delantero',
+            lugarNacimiento: 'Santa Fe, Argentina',
+            edad: '34',
+            img: 'https://media.tycsports.com/files/2022/03/27/407745/angel-di-maria_416x234.jpg'
+        },
+        {
+            id: '21',
+            dorsal: '10',
+            nombreCompleto: 'Lionel Andrés Messi',
+            posicion: 'Delantero',
+            lugarNacimiento: 'Santa Fe, Argentina',
+            edad: '34',
+            img: 'https://phantom-marca.unidadeditorial.es/e1e65aab8cbcb632d9c8359b2b6840f9/resize/1320/f/jpg/assets/multimedia/imagenes/2021/07/17/16265320344770.jpg'
+        },
+        {
+            id: '22',
+            dorsal: '22',
+            nombreCompleto: 'Lautaro Javier Martinez',
+            posicion: 'Delantero',
+            lugarNacimiento: 'Buenos Aires, Argentina',
+            edad: '24',
+            img: 'https://as01.epimg.net/futbol/imagenes/2021/09/08/internacional/1631131667_666808_1631132007_noticia_normal_recorte1.jpg'
+        },
+        {
+            id: '23',
+            dorsal: '16',
+            nombreCompleto: 'Carlos Joaquín Correa',
+            posicion: 'Delantero',
+            lugarNacimiento: 'Tucuman, Argentina',
+            edad: '27',
+            img: 'https://media.tycsports.com/files/2021/08/26/324405/joaquin-correa-inter_1440x810_wmk.jpg'
+        },
+        {
+            id: '24',
+            dorsal: '21',
+            nombreCompleto: 'Ángel Martín Correa Martínez',
+            posicion: 'Delantero',
+            lugarNacimiento: 'Santa Fe, Argentina',
+            edad: '27',
+            img: 'https://elintranews.com/wp-content/uploads/2022/01/Angel-Correa.png'
+        },
+        {
+            id: '25',
+            dorsal: '9',
+            nombreCompleto: 'Julián Álvarez',
+            posicion: 'Delantero',
+            lugarNacimiento: 'Cordoba, Argentina',
+            edad: '22',
+            img: 'https://media.ambito.com/p/87c93135add3d3c5570f1082f039b46b/adjuntos/239/imagenes/039/602/0039602800/julian-alvarez-aranajpg.jpg'
+        }
+    ]
+
+const primeraCarga = () =>{
+
+    scaloneta.forEach(j =>{
+        const card = 
+        `
+        <div class="col-sm-6">
+            <div class="card" style="width: 15rem;">
+                <img src= "${j.img}" class="card-img-top" alt="5">
+                <div class="card-body">
+                    <h5 class="card-title">${j.nombreCompleto}</h5>
+                    <p class="card-text">
+                        Posicion: ${j.posicion}<br>
+                        Dorsal: ${j.dorsal}<br>
+                        Edad: ${j.edad}<br>
+                        Lugar de Nacimiento: ${j.lugarNacimiento}
+                    </p>
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("card").insertAdjacentHTML('beforeend',card);
+    });
+};
+
+window.onload = primeraCarga();
+
+const buscarPorId = () =>{
+
+    let idABuscar = document.getElementById("idJugador").value;
+    let jugador = scaloneta.find(x => x.id == idABuscar);
+    const card = 
+    `
+    <div class="col-sm-6">
+        <div class="card" style="width: 15rem;">
+            <img src= "${jugador.img}" class="card-img-top" alt="5">
+            <div class="card-body">
+                <h5 class="card-title">${jugador.nombreCompleto}</h5>
+                <p class="card-text">
+                    Posicion: ${jugador.posicion}<br>
+                    Dorsal: ${jugador.dorsal}<br>
+                    Edad: ${jugador.edad}<br>
+                    Lugar de Nacimiento: ${jugador.lugarNacimiento}
+                </p>
+            </div>
+        </div>
+    </div>
+    `
+
+    document.getElementById("card").innerHTML = "";
+    document.getElementById("card").insertAdjacentHTML('beforeend',card);
+};
+
+const buscarPorPosicion = () =>{
+
+    let posicionABuscar = document.getElementById("idPosicion").value;
+
+    let jugadores = scaloneta.filter(x => x.posicion == posicionABuscar);
+
+    document.getElementById("card").innerHTML = "";
+
+    jugadores.forEach(j => {
+        const card = 
+        `
+        <div class="col-sm-6">
+            <div class="card" style="width: 15rem;">
+                <img src= "${j.img}" class="card-img-top" alt="5">
+                <div class="card-body">
+                    <h5 class="card-title">${j.nombreCompleto}</h5>
+                    <p class="card-text">
+                        Posicion: ${j.posicion}<br>
+                        Dorsal: ${j.dorsal}<br>
+                        Edad: ${j.edad}<br>
+                        Lugar de Nacimiento: ${j.lugarNacimiento}
+                    </p>
+                </div>
+            </div> 
+        </div>
+        `
+        document.getElementById("card").insertAdjacentHTML('beforeend',card);
+    })
+        
+};
+
+const nuevoJugador = () =>{
+    let nombre = document.getElementById("nuevoNombre"). value;
+    let posicion = document.getElementById("nuevaPosicion"). value;
+    let dorsal = document.getElementById("nuevaDorsal"). value;
+    let lugarNac = document.getElementById("nuevoLugarNac"). value;
+    let edad = document.getElementById("nuevaEdad"). value;
+
+    let jugadorNuevo = {
+        nombre: nombre,
+        posicion: posicion,
+        dorsal: dorsal,
+        lugarNacimiento: lugarNac,
+        edad, edad
+    }
+    console.log(jugadorNuevo)
+}
